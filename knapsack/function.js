9f4396a912a7dc151e795e45b5ef41dcc97d3455
DendyAR <anugerah700@gmail.com>
2021-06-30 13:00:52 +0700

    initial commit

--- knapsack/function.js
+++ knapsack/function.js
@@ -1,7 +1,56 @@
 class Knapsack {
-    backpack(w, v , n) {
-        
+    constructor(berat,harga,capacity) {
+        this.berat = berat
+        this.harga = harga
+        this.capacity = capacity
+    }
+
+    
+
+    max(hasil, add){
+        let temp1, temp2
+        for (let i = 1; i < hasil.length; i++) {
+            let j = i
+            temp1 = hasil[i]
+            temp2 = add[i]
+            while (j > 0 && temp1 < hasil[j - 1]) {
+                hasil[j] = hasil[j - 1]
+                add[j] = add[j - 1]
+                j--
+            }
+            hasil[j] = temp1
+            add[j] = temp2
+        }
+        console.log(hasil)
+        console.log(add)
+        return `Dapat Dimasukan : ${add[add.length-1].split('')} (kg) \n harga : Rp. ${hasil[hasil.length-1]}`
+    }
+
+    knapSack() {
+        let berat = this.berat
+        let harga = this.harga
+        let capacity = this.capacity
+
+        let add = []
+        let hasil = []
+        let f = (s, berat, sp, harga, count) => {
+            for(let i=0; i<berat.length; i++){
+
+                if(s  + berat[i] <= capacity){
+                    add.push(`${sp}${berat[i]}`)
+                    hasil.push(`${count  + harga[i]}`)
+                }
+                f(s + berat[i], berat.slice(i+1), sp + berat[i], harga.slice(i+1), count + harga[i])
+
+            }
+        }
+        f(0, berat, '', harga, 0)
+        console.log(this.max(hasil, add))
+    }
+
+    choose() {
+        this.knapSack()
     }
 }
 
-Module.exports= Knapsack
+module.exports=Knapsack
--- knapsack/index.js
+++ knapsack/index.js
@@ -1,8 +1,35 @@
-const Knapsack = require('./function')
+const knapsack = require('./function')
 
-let w = []
-let v = []
+let v = [
+    {
+        nama : "ABC",
+        berat : 2,
+        harga : 2600000
+    },
+    {
+        nama : "DEF",
+        berat : 3,
+        harga : 2600000
+    },
+    {
+        nama : "GHI",
+        berat : 2,
+        harga : 2450000
+    },
+    {
+        nama : "JKL",
+        berat : 4,
+        harga : 4800000
+    },
+    {
+        nama : "MNO",
+        berat : 5,
+        harga : 5100000
+    },
+]
 
-let p = new Knapsack()
-let hasil = p.backpack(w, v )
-console.log(hasil)
+let berat = v.map(x => {return x.berat}).sort();
+let harga = v.map(z => {return z.harga}).sort()
+let capacity = 7
+let result = new knapsack(berat,harga,capacity);
+result.choose()
